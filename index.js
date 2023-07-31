501108bf063ed3198004febfb6b730f29868c4ff
Nancy D. Arena <ndarena11@gmail.com>
2023-07-31 01:50:34 -0400

    removing innerHTML in renderGames

--- index.js
+++ index.js
@@ -8,44 +8,48 @@ function fetchData(){
 
 function renderGames(games){
     games.forEach((game) => {
-        const container = document.querySelector(".game-container");
+        const container = document.querySelector("#game-container");
         const gameDiv = document.createElement("div");
+        container.append(gameDiv);
         gameDiv.className = "game";
         gameDiv.id = game.id;
-        gameDiv.innerHTML = `<h2>${game.name}</h2>
-        <img src="${game.image}" alt="${game.name}">
-        <p>Price: $${game.price}</p>
-        <p>Release: ${game.release}</p>
-        <p>Genre: ${game.genre}</p>
-        <p>Mode: ${game.mode}</p>`
-        container.append(gameDiv);
+
+        const gameName = document.createElement("h2");
+        gameName.textContent = game.name;
+        gameDiv.append(gameName);
+
+        const img = document.createElement("img");
+        img.src = game.image;
+        img.alt = game.name;
+        gameDiv.append(img);
+       
+        const price = document.createElement("p");
+        price.textContent = `Price: $${game.price}`;
+        gameDiv.append(price);
+
+        const release = document.createElement("p");
+        release.textContent = `Release: ${game.release}`;
+        gameDiv.append(release);
+
+        const genre = document.createElement("p");
+        genre.textContent = `Genre: ${game.genre}`;
+        gameDiv.append(genre);
+
+        const mode = document.createElement("p");
+        mode.textContent = `Mode: ${game.mode}`;
+        gameDiv.append(mode);
+
         const btn = document.createElement("button");
         btn.id = "buybtn";
         btn.textContent = "Add to Cart";
-        btn.addEventListener("click", (btn) => alert('Added to cart!'));
+        btn.addEventListener("click", btn);
         gameDiv.append(btn);
     });
 }
 
-function searchGames() {
-     let form = document.querySelector("form");
-     form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let input = document.querySelector("input").value;
-        let games = document.querySelectorAll(".game");
-        for (const game of games) {
-            if (input === game.name) {
-                renderGames(game);
-            } else {
-                console.log("No match found!");
-            }
-        }   
-     })
-};
 
 
 
 
 
-                
 
