ba61e821053af23715ac4dd19198e24d4cc19db6
Nancy D. Arena <ndarena11@gmail.com>
2023-08-13 13:13:42 -0400

    adding global variables and class Storage

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ function fetchGames(){
         renderGames(games);
         Storage.saveGames(games);
     });
-}
+};
+
+//Global variables
+let cart = [];
+const cartQuantity = document.querySelector("#cart-quantity");
+const toggleCart = document.getElementById("toggleCart");
+const cartTotalValue = document.getElementById('cart-total-value');
+
+class Storage {
+    static saveGames(games){
+        localStorage.setItem("games", JSON.stringify(games));
+    };
+    static getGames(gameId) {
+        const games = JSON.parse(localStorage.getItem("games"));
+        return games.find((game) => game.id === gameId);
+    };
+    static saveCartItems(cart){
+        localStorage.setItem("cart", JSON.stringify(cart));
+    };
+    static getCartItems(){
+        return JSON.parse(localStorage.getItem("cart")) || [];
+    };
+};
+
