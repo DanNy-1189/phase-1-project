88974f6da3187d428dfe75fa53509f5d36a694dd
Nancy D. Arena <ndarena11@gmail.com>
2023-08-13 13:09:40 -0400

    sending games data to local storage and renderGames

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => fetchGames())
 
 
+function fetchGames(){
+    fetch("http://localhost:3000/games")
+    .then((resp) => resp.json())
+    .then((games) => {
+        renderGames(games);
+        Storage.saveGames(games);
+    });
+}
